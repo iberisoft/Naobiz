8ab5d7820e60fa24e3b4d3b5f9c884586d4c7ffb
Pavel Zaytsev <pavel@iberisoft.com>
2020-04-02 19:46:23 +0200

    Enable logging

--- Naobiz/Program.cs
+++ Naobiz/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Naobiz
 {
@@ -12,6 +13,10 @@ namespace Naobiz
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>()
