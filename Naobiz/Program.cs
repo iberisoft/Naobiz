5c9b87ca75e29b1d795d51f3d4539e639158f29e
Pavel Zaytsev <pavel@iberisoft.com>
2020-05-17 17:33:16 +0200

    Resolve #28 "Run as service"

--- Naobiz/Program.cs
+++ Naobiz/Program.cs
@@ -1,22 +1,48 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System;
+using Topshelf;
 
 namespace Naobiz
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var exitCode = HostFactory.Run(hostConfig =>
+            {
+                hostConfig.Service<Service>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(() => new Service(CreateHostBuilder(args).Build()));
+                    serviceConfig.WhenStarted(service => service.Start());
+                    serviceConfig.WhenStopped(service => service.Stop());
+                });
+                hostConfig.RunAsLocalService();
+                hostConfig.SetServiceName("Naobiz");
+                hostConfig.SetDescription("E-commerce portal");
+            });
+            return (int)exitCode;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+                .UseContentRoot(AppDomain.CurrentDomain.BaseDirectory)
                 .UseSerilog((hostContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        class Service
+        {
+            readonly IHost m_Host;
+
+            public Service(IHost host) => m_Host = host;
+
+            public void Start() => m_Host.Start();
+
+            public void Stop() => m_Host.Dispose();
+        }
     }
 }
