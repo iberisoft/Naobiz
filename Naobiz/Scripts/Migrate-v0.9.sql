5a9286e99cd7c69786c826291d5473c4030600c7
Pavel Zaytsev <pavel@iberisoft.com>
2020-07-25 14:36:05 +0200

    Add a migration script

---
+++ Naobiz/Scripts/Migrate-v0.9.sql
@@ -0,0 +1,46 @@
+﻿-- Extend ChatMessages.Text
+
+ALTER TABLE [ChatMessages] ALTER COLUMN [Text] nvarchar(250)
+
+GO
+
+-- Add Users.Company
+
+ALTER TABLE [Users] ADD [Company] nvarchar(100)
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Users_Company] ON [Users] 
+(
+	[Company] ASC
+)
+
+GO
+
+-- Add ForumGroups.OrderNumber
+
+ALTER TABLE [ForumGroups] ADD [OrderNumber] [int]
+
+GO
+
+UPDATE [ForumGroups] SET [OrderNumber] = 0
+
+GO
+
+ALTER TABLE [ForumGroups] ALTER COLUMN [OrderNumber] [int] NOT NULL
+
+GO
+
+-- Add ResourceTypes.OrderNumber
+
+ALTER TABLE [ResourceTypes] ADD [OrderNumber] [int]
+
+GO
+
+UPDATE [ResourceTypes] SET [OrderNumber] = 0
+
+GO
+
+ALTER TABLE [ResourceTypes] ALTER COLUMN [OrderNumber] [int] NOT NULL
+
+GO
