iberisoft/Naobiz

commit 1681468ef38292ed4ccccc575118f0c85a12fe1b

Fix a bug

Naobiz/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using System;
 using Topshelf;
 
 namespace Naobiz
@@ -27,7 +26,6 @@ namespace Naobiz
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .UseContentRoot(AppDomain.CurrentDomain.BaseDirectory)
                 .UseSerilog((hostContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {

commit 88b4970fdced22328fa6d057e0d7c89e252f7109

Fix a bug introduced in 1681468e
It occurs when running as Windows service.

Naobiz/Program.cs
@@ -29,6 +29,9 @@ namespace Naobiz
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+#if !DEBUG
+                .UseContentRoot(System.AppDomain.CurrentDomain.BaseDirectory)
+#endif
                 .UseSerilog((hostContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {